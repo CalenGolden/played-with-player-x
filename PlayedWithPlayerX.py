942736e7e294eb390454cc5d09345ed7d92e9313
unknown <cagolden@calpoly.edu>
2021-10-08 13:26:48 -0700

    Up to date code: 20 matches

--- PlayedWithPlayerX.py
+++ PlayedWithPlayerX.py
@@ -4,18 +4,20 @@ import requests
 #JSON request for the summoner data of the entered player to get the playerID
 def requestSummonerData(region, summonerName, APIKey):
     URL = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + APIKey
+    print(URL)
     response = requests.get(URL)
     return response.json()
 
 #Uses the above playerID to access the player's match history
 def requestMatchHistory(region, ID, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + ID + "?api_key=" + APIKey
+    URL = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + ID + "/ids?start=0&count=50&api_key=" + APIKey
+    print(URL)
     response = requests.get(URL)
     return response.json()
 
 #Retrieves the data of a specific match from the match history
 def requestMatch(region, gameId, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/match/v4/matches/" + gameId + "?api_key=" + APIKey
+    URL = "https://americas.api.riotgames.com/lol/match/v5/matches/" + gameId + "?api_key=" + APIKey
     response = requests.get(URL)
     return response.json()
 
@@ -30,28 +32,35 @@ def main():
     #Prompting the user to enter their region, summoner name, and APIKey for accessing the JSON files
     region = (str)(input('Type in one of the regions above: '))
     summonerName = (str)(input('Type your Summoner Name here and DO NOT INCLUDE ANY SPACES: '))
-    checkName = (str)(input('Type the Summoner Name you wish to check for: '))
+    checkName = ((str)(input('Type the Summoner Name you wish to check for: '))).replace(" ", "%")
     APIKey = (str)(input('Copy and paste your API Key here: '))
-    responseJSON  = requestSummonerData(region, summonerName, APIKey)
+    summonerDataInfo  = requestSummonerData(region, summonerName, APIKey)
 
     #JSON request for player match history
-    ID = responseJSON['accountId']
-    ID = str(ID)
-    responseJSON2 = requestMatchHistory(region, ID, APIKey)
+    playerID = summonerDataInfo['puuid']
+    playerID = str(playerID)
+    matchHistoryInfo = requestMatchHistory(region, playerID, APIKey)
+
+    checkerResponse  = requestSummonerData(region, checkName, APIKey)
+    checkerID = checkerResponse['puuid']
+    checkerID = str(checkerID)
+
+
 
     #Iteratively loop through the match history to check for the specified summoner name
-    for x in range(50):
-        gameId = responseJSON2['matches'][x]['gameId']
+    for x in range(20):
+        gameId = matchHistoryInfo[x]
         gameId = str(gameId)
-        responseJSON3 = requestMatch(region, gameId, APIKey)
+        matchInfo = requestMatch(region, gameId, APIKey)
         i = 0
 
         for i in range(10):
-            if responseJSON3['participantIdentities'][i]['player']['summonerName'] == checkName:
+            if matchInfo['metadata']['participants'][i] == checkerID:
                 count += 1
+                break
 
     count = str(count)
-    print("In the last 50 games you have played with " + checkName + " " + count + " time(s)!") 
+    print("In the last 20 games you have played with " + checkName + " " + count + " time(s)!")
 
 
 if __name__ == "__main__":
